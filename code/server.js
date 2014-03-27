3fe9807b0e999b0a933368daf5d8bbdb0688bcec
John DiLeo <jdileo4>
2014-03-26 23:03:53 -0500

    began server.js code, have event handling for moveLeft, passes it back down to clients.

--- code/server.js
+++ code/server.js
@@ -1,4 +1,6 @@
-// This node.js program implements a simple chat room service.
+//-----------------------------------------
+//setup
+//-----------------------------------------
 
 // The node.js HTTP server.
 var app = require('http').createServer(handler);
@@ -7,11 +9,11 @@ var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 
 // Allows access to local file system.
-var fs = require('fs');
-
+var fs = require('fs')
 
 // Listen on a high port.
-app.listen(20202);
+app.listen(30303);
+
 
 // Handles HTTP requests.
 function handler(request, response) {
@@ -19,7 +21,7 @@ function handler(request, response) {
   // argument) to process the content of the file.
   // __dirname is a preset variable pointing to the folder of this file.
   fs.readFile(
-    __dirname + '/../index.html',
+    __dirname + '/index.html',
     function(err, content) {
       if (err) {
         // If an error happened when loading 'index.html', return a 500 error.
@@ -32,68 +34,83 @@ function handler(request, response) {
     });
 }
 
-// Tells socket.io to listen to an event called 'connection'.
-// This is a built-in event that is triggered when a client connects to the
-// server. At that time, the function (the 2nd argument) will be called with an
-// object representing the client.
-io.sockets.on(
-  'connection',
-  function(client) {
-    // Send a welcome message first.
-    client.emit('welcome', 'Welcome to Bitris!');
+//-----------------------------------------
+//main code
+//-----------------------------------------
 
-    // Listen to an event called 'login'. The client should emit this event when
-    // it wants to log in to the chat room.
-    client.on(
-      'login',
-      function(message) {
-        // This function extracts the user name from the login message, stores
-        // it to the client object, sends a login_ok message to the client, and
-        // sends notifications to other clients.
-        if (message && message.user_name) {
-          client.set('user_name', message.user_name);
-          client.emit('login_ok');
-          // client.broadcast.emits() will send to all clients except the
-          // current client. See socket.io FAQ for more examples.
-          client.broadcast.emit('notification',
-                                message.user_name + ' entered the room.');
-          return
-        }
-        // When something is wrong, send a login_failed message to the client.
-        client.emit('login_failed');
-      });
+var globalGrid = [][];
 
-    // Listen to an event called 'chat'. The client should emit this event when
-    // it sends a chat message.
-    client.on(
-      'chat',
-      function(message) {
-        // This function tries to get the user name from the client object, and
-        // use that to form a chat message that will be sent to all clients.
-        if (message && message.msg) {
-          client.get(
-            'user_name', 
-            function(err, name) {
-              if (!err) {
-                // io.sockets.emit() will send the message to all clients,
-                // including the current client. See socket.io FAQ for more
-                // examples.
-                io.sockets.emit('chat', { user_name: name, msg: message.msg });
-              }
-            });
-        }
-      });
+var player1Piece = Piece();
+var player2Piece = Piece();
 
-    // Print a message when somebody left.
-    client.on(
-      'disconnect',
-      function() {
-        client.get(
-          'user_name',
-          function(err, name) {
-            if (name) {
-              io.sockets.emit('notification', name + ' left the room.');
-            }
-          });
-      });
-  });
+//make sure globalGrid is up to date
+var updateGlobalGrid = function()
+{
+	for (var x = 0; x<17; x++)
+	{
+		for (var y = 0; y < 17; y++)
+		{
+			if ( player1Piece.lastLocations[0].x = x &&
+					player1Piece.lastLocations[0].y = y)
+			{
+				globalGrid[x][y] = 0;
+			}
+			if ( player1Piece.locations[0].x = x &&
+					player1Piece.locations[0].y = y)
+			{
+				globalGrid[x][y] = player1Piece.color;
+			}
+		}
+	}
+}
+
+
+//listeners
+
+socket.on(
+	'moveLeft',
+	function()
+	{
+		player1Piece.moveLeft()
+		socket.broadcast.emit('syncUpdate', globalGrid);
+	};
+);
+
+
+
+
+
+//-------------------------------------------
+//Piece class
+//-------------------------------------------
+
+function Piece(){
+	this.color = 1;
+	if(this.locations)
+	{
+		this.lastLocations = this.locations;
+	}
+	else
+	{
+		this.lastLocations = [];
+	}
+	this.locations = [];
+	this.locations[0] = {
+	//just over top right of play area
+		x: 13,
+		//y increases downward
+		y: 0
+	}
+	
+	updateGlobalGrid();
+	
+	
+}
+
+Piece.prototype.moveLeft = function()
+{
+	this.lastLocations[0] = this.locations[0];
+	this.locations[0].x--;
+	updateGlobalGrid();
+}
+
