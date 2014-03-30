f7b1df161f8481633c14373f70dcee937d227a2b
John DiLeo <jdileo4>
2014-03-29 20:34:56 -0500

    Back-end is working.  Hears move commands and sends down updated 2d grid to clients.  Had to add some stuff to .html file, but the things I changed I labeled with a comment: SCOTT  and a description of what I changed/why.   so just ctrl-f that for my changes.

--- code/server.js
+++ code/server.js
@@ -14,26 +14,24 @@ var fs = require('fs');
 // Listen on a high port.
 app.listen(30303);
 
-
 // Handles HTTP requests.
 function handler(request, response) {
-  // This will read the file 'index.html', and call the function (the 2nd
-  // argument) to process the content of the file.
-  // __dirname is a preset variable pointing to the folder of this file.
-  fs.readFile(
-    __dirname + '/../Bitris.html',
-    function(err, content) {
-      if (err) {
-        // If an error happened when loading 'index.html', return a 500 error.
-        response.writeHead(500);
-        return response.end('Error loading index.html!');
-      }
-      // If no error happened, return the content of 'index.html'
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.end(content);
-    });
+	// This will read the file 'index.html', and call the function (the 2nd
+	// argument) to process the content of the file.
+	// __dirname is a preset variable pointing to the folder of this file.
+	fs.readFile(__dirname + '/../Bitris.html', function(err, content) {
+		if (err) {
+			// If an error happened when loading 'index.html', return a 500 error.
+			response.writeHead(500);
+			return response.end('Error loading index.html!');
+		}
+		// If no error happened, return the content of 'index.html'
+		response.writeHead(200, {
+			'Content-Type' : 'text/html'
+		});
+		response.end(content);
+	});
 }
-
 
 // Tells socket.io to listen to an event called 'connection'.
 // This is a built-in event that is triggered when a client connects to the
@@ -41,42 +39,84 @@ function handler(request, response) {
 // object representing the client.
 
 //(when someone connects, add these event handlers to that client)
-io.sockets.on(
-  'connection',
-  function(client) {
-    // Send a welcome message first.
-    client.emit('welcome', 'Welcome to my chat room!');
+var allsockets = [];
+io.sockets.on('connection', function(client) {
+	allsockets.push(client);
+	// Send a welcome message first.
+	client.emit('welcome', 'Welcome to Bitris!');
 
-    // Listen to an event called 'login'. The client should emit this event when
-    // it wants to log in to the chat room.
-    client.on(
-      'login',
-      function(message) {
-        // This function extracts the user name from the login message, stores
-        // it to the client object, sends a login_ok message to the client, and
-        // sends notifications to other clients.
-        if (message && message.user_name) {
-          client.set('user_name', message.user_name);
-          client.emit('login_ok');
-          // client.broadcast.emits() will send to all clients except the
-          // current client. See socket.io FAQ for more examples.
-          client.broadcast.emit('notification',
-                                message.user_name + ' entered the room.');
-			
+	// Listen to an event called 'login'. The client should emit this event when
+	// it wants to log in to the chat room.
+	client.on('login', function(message) {
+	
+		//debug
+		//console.log("login sent from html and picked up by listener in server");
+		// This function extracts the user name from the login message, stores
+		// it to the client object, sends a login_ok message to the client, and
+		// sends notifications to other clients.
+		if (message && message.user_name) {
+			client.set('user_name', message.user_name);
+			client.emit('login_ok');
+			// client.broadcast.emits() will send to all clients except the
+			// current client. See socket.io FAQ for more examples.
+			client.broadcast.emit('notification', message.user_name + ' entered the room.');
+
 			//added------------------------------
-			//adding piece to client
-			//TODO
-			
-          return
-        }
-        // When something is wrong, send a login_failed message to the client.
-        client.emit('login_failed');
-      });
+			client.set('piece', new Piece());
+
+			return;
+		}
+		// When something is wrong, send a login_failed message to the client.
+		client.emit('login_failed');
+	});
+	
+	
+	//listeners.............
+
+	//TODO is it socket.on or something else?  or this?
+	client.on('moveLeft', function() {
+		//debug
+		console.log("moveLeft sent from html and picked up by listener in server");
+		
+		//TODO  is this right?  does this detect 'moveLeft' from
+		//either client then access the piece from only that client
+		//that sent the message 'moveLeft'?
+		client.get('piece',
+				function(err, result)
+				{
+					result.moveLeft();
+				}
+			);
+		io.sockets.emit('syncUpdate', globalGrid);
+	});
+
+	client.on('moveRight', function() {
+		//TODO  is this right?  does this detect 'moveRight' from
+		//either client then access the piece from only that client
+		//that sent the message 'moveRight'?
+		client.get('piece', 
+			function(err, result)
+			{
+				result.moveRight();
+			}
+		);
+		io.sockets.emit('syncUpdate', globalGrid);
+	});
+
+	client.on('moveDown', function() {
+		//TODO  is this right?  does this detect 'moveDown' from
+		//either client then access the piece from only that client
+		//that sent the message 'moveDown'?
+		client.get('piece', 
+			function(err, result)
+			{
+				result.moveDown();
+			}
+		);
+		io.sockets.emit('syncUpdate', globalGrid);
+	});
+
 });
-
-
-
-
 
 //-----------------------------------------
 //main code
@@ -84,124 +124,83 @@ io.sockets.on(
 
 //make 2 dimensional 17x17 array
 var globalGrid = new Array(17);
-  for (var i = 0; i < 17; i++) {
-    globalGrid[i] = new Array(17);
-  }
-
-var player1Piece = Piece();
-var player2Piece = Piece();
-
-//make sure globalGrid is up to date
-var updateGlobalGrid = function()
-{
-	for (var x = 0; x<17; x++)
-	{
-		for (var y = 0; y < 17; y++)
-		{
-			if ( player1Piece.lastLocations[0].x == x &&
-					player1Piece.lastLocations[0].y == y)
-			{
-				globalGrid[x][y] = 0;
-			}
-			if ( player1Piece.locations[0].x == x &&
-					player1Piece.locations[0].y == y)
-			{
-				globalGrid[x][y] = player1Piece.color;
-			}
-		}
-	}
+for (var i = 0; i < 17; i++) {
+	globalGrid[i] = new Array(17);
 }
 
 
-//listeners.............
+//make sure globalGrid is up to date
+function updateGlobalGrid() {
 
-socket.on(
-	'moveLeft',
-	function()
+	for (var i = 0; i < allsockets.length; ++i)
 	{
-	//how do we know which player is emitting this event?
-		//if 'moveLeft' came from player1
-		player1Piece.moveLeft();
-		//if 'moveLeft' came from player2
-		//player2Piece.moveLeft()
-		socket.broadcast.emit('syncUpdate', globalGrid);
-	}
-);
-
-
-socket.on(
-	'moveRight',
-	function()
-	{
-	//how do we know which player is emitting this event?
-		//if 'moveRight' came from player1
-		
-		player1Piece.moveRight();
-		//if 'moveRight' came from player2
-		//player2Piece.moveRight()
-		socket.broadcast.emit('syncUpdate', globalGrid);
-	}
-);
-socket.on(
-	'moveDown',
-	function()
-	{
-	//how do we know which player is emitting this event?
-		//if 'moveDown' came from player1
-		player1Piece.moveDown();
-		//if 'moveDown' came from player2
-		//player2Piece.moveDown()
-		socket.broadcast.emit('syncUpdate', globalGrid);
-	}
-);
+	    client = allsockets[i];
+		client.get('piece', function(err, result) 
+		{
+			for (var x = 0; x < 17; x++) {
+				for (var y = 0; y < 17; y++) {
+					if (result.lastLocations[0].x == x && result.lastLocations[0].y == y) 
+					{
+						globalGrid[x][y] = 0;
+					};
+					if (result.locations[0].x == x && result.locations[0].y == y) 
+					{
+						globalGrid[x][y] = result.color;
+					};
+				};
+			};
+		});
+	};
+};
 
 
 //-------------------------------------------
 //Piece class
 //-------------------------------------------
 
-function Piece(){
+function Piece() {
 	this.color = 1;
-	if(this.locations)
-	{
-		this.lastLocations = this.locations;
-	}
-	else
-	{
-		this.lastLocations = [];
-	}
 	this.locations = [];
 	this.locations[0] = {
-	//just over top right of play area
-		x: 13,
+		//just over top right of play area
+		x : 13,
 		//y increases downward
-		y: 0
+		y : 0
+	};
+	
+	this.lastLocations = this.locations;
+
+	//update globalGrid
+	for (var x = 0; x < 17; x++) {
+		for (var y = 0; y < 17; y++) {
+			if (this.lastLocations[0].x == x && this.lastLocations[0].y == y) {
+				globalGrid[x][y] = 0;
+			}
+			if (this.locations[0].x == x && this.locations[0].y == y) {
+				globalGrid[x][y] = this.color;
+			}
+		}
 	}
-	
-	updateGlobalGrid();
-	
-	
 }
 
-Piece.prototype.moveLeft = function()
-{
+//move functions
+
+Piece.prototype.moveLeft = function() {
 	this.lastLocations[0] = this.locations[0];
 	this.locations[0].x--;
 	updateGlobalGrid();
-}
+};
 
-Piece.prototype.moveRight = function()
-{
+Piece.prototype.moveRight = function() {
 	this.lastLocations[0] = this.locations[0];
 	this.locations[0].x++;
 	updateGlobalGrid();
-}
+};
 
-Piece.prototype.moveDown = function()
-{
+Piece.prototype.moveDown = function() {
 	this.lastLocations[0] = this.locations[0];
 	this.locations[0].y++;
-	//TODO reset clock
+	//future... reset clock
 	updateGlobalGrid();
-}
+};
 
